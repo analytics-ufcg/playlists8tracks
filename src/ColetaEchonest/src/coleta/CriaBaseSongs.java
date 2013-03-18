6d4d83cbb20f8a45f8bc3f1464ab0215f776b2b7
Ricardo Araújo <rikardoas@gmail.com>
2013-03-18 14:24:05 -0300

    updated exception handling

--- src/ColetaEchonest/src/coleta/CriaBaseSongs.java
+++ src/ColetaEchonest/src/coleta/CriaBaseSongs.java
@@ -1,10 +1,9 @@
 package coleta;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,13 +16,12 @@ import com.echonest.api.v4.Song;
 
 public class CriaBaseSongs {
 	private EchoNestAPI en, en1, en2, en3;
-//	final static Charset ENCODING = StandardCharsets.ISO_8859_1;
+	//	final static Charset ENCODING = StandardCharsets.ISO_8859_1;
 	final static String PASTA_TIMBRE = "timbres/";
 	int indexAtual;
 
 	public CriaBaseSongs() {
-		final String[] APIKeys = { "WFY3BMON2KYKZG19U", "ILKZUHGM3CMJKJ9OI",
-				"8DBZK8RH9HZBGUERB" };
+		final String[] APIKeys = { "WFY3BMON2KYKZG19U", "ILKZUHGM3CMJKJ9OI", "8DBZK8RH9HZBGUERB" };
 		en1 = new EchoNestAPI(APIKeys[0]);
 		en2 = new EchoNestAPI(APIKeys[1]);
 		en3 = new EchoNestAPI(APIKeys[2]);
@@ -46,16 +44,14 @@ public class CriaBaseSongs {
 			en = en3;
 			indexAtual = 2;
 		} else {
-			en = en1;
-			indexAtual = 0;
+			throw new RuntimeException("No more valid keys!");
 		}
 
 	}
 
 	private static Song identificaCancao(List<Song> songs, String artist) {
 		for (Song s : songs) {
-			if (s.getArtistName().toLowerCase()
-					.contains(artist.toLowerCase().trim())) {
+			if (s.getArtistName().toLowerCase().contains(artist.toLowerCase().trim())) {
 				return s;
 			}
 		}
@@ -63,176 +59,146 @@ public class CriaBaseSongs {
 
 	}
 
-	private String searchSongByTitleAndArtist(String title, String artist, String songID) {
+	private String searchSongByTitleAndArtist(String title, String artist, String songID) throws EchoNestException{
 		ArtistParams ap = new ArtistParams();
 		Params p = new Params();
 
 		ap.addName(artist);
 		List<Song> songs;
-		String resultado = "";
-		try {
-			p.add("artist", artist);
-			p.add("title", title);
-			p.add("results", 50);
+		p.add("artist", artist);
+		p.add("title", title);
+		p.add("results", 50);
+
+		songs = en.searchSongs(p);
+
+		Song song = identificaCancao(songs, artist);
+
+		if (song != null) {
+			String arquivoTimbre = PASTA_TIMBRE + songID + ".txt";;
+			String resultado = String.format(
+					"%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%d\t%s",
+					song.getSongHotttnesss(),
+					song.getTempo(), song.getDanceability(),
+					song.getEnergy(), song.getLoudness(),
+					song.getDuration(), song.getTimeSignature(),
+					arquivoTimbre);
+
+			FileWriter writer;
 			try {
-				songs = en.searchSongs(p);
-			} catch (Exception e) {
-				System.out.println("QUEBROU SONGS");
-				System.out.println("indice atual eh " + indexAtual);
-				getProximaAPIKey();
-				songs = en.searchSongs(p);
-			}
-			
-			Song song = identificaCancao(songs, artist);
-
-			if (song != null) {
-				String arquivoTimbre = PASTA_TIMBRE + songID + ".txt";;
-				resultado = String.format(
-						"%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%d\t%s",
-						song.getSongHotttnesss(),
-						song.getTempo(), song.getDanceability(),
-						song.getEnergy(), song.getLoudness(),
-						song.getDuration(), song.getTimeSignature(),
-						arquivoTimbre);
-
-				FileWriter fstream;
-				try {
-					fstream = new FileWriter(arquivoTimbre);
-					BufferedWriter saida = new BufferedWriter(fstream);
-					String textoTimbre = getTimbres(song);
-					saida.write(textoTimbre);
-					saida.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			} else {
-				return "";
-			}
-
-		} catch (EchoNestException e) {
-
-			e.printStackTrace();
-		}
-		return resultado;
-
-	}
-
-	private static String transformaArrayString(double[] d) {
-		String result = "";
-		for (int i = 0; i < d.length; i++) {
-			result += d[i] + ",";
-		}
-		return result.substring(0, result.length() - 1);
-	}
-
-	private static String getTimbres(Song song) {
-		String resultado = "";
-		try {
-			
-			if (song.getAnalysisURL() == null){
+				writer = new FileWriter(arquivoTimbre);
+				writer.write(getTimbres(song));
+				writer.flush();
+				writer.close();
 				return resultado;
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
 			}
-			
-			List<Segment> segmentos = song.getAnalysis().getSegments();
-			for (Segment s : segmentos) {
-				resultado += transformaArrayString(s.getTimbre()) + "\n";
-			}
-		} catch (EchoNestException e) {
-			System.out.println("Erro ao pegar segmentos");
-			e.printStackTrace();
+
 		}
-		return resultado;
+
+		return "";
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	private static String getTimbres(Song song) throws EchoNestException {
 		
+		try{
+			StringBuilder resultado = new StringBuilder();
+			List<Segment> segmentos = song.getAnalysis().getSegments();
+			
+			if(segmentos == null){
+				return resultado.toString();
+			}
+
+			for (Segment s : segmentos) {
+				resultado.append(Arrays.toString(s.getTimbre()));
+				resultado.append('\n');
+			}
+			
+			return resultado.toString().replaceAll("\\[|\\]", "").replaceAll(",\\s", ",");
+		}catch(Exception e){
+			e.printStackTrace();
+			return "";
+		}
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+
 		CriaBaseSongs sse = new CriaBaseSongs();
-		
+
 		String inputFileName = args[0];
 		String outputFileName = args[1];
 		String errorFileName = args[2];
-		
+
 		Scanner input = new Scanner(new File(inputFileName));
-		
 		int lineNumber = 0;
-		
+
 		if(new File("contagem.txt").exists()){
 			lineNumber = new Scanner(new File("contagem.txt")).nextInt();
 		}
-		
-		try {
-			int currentLine = 0;
-			while(currentLine < lineNumber){
-				if(input.hasNextLine()){
-					input.nextLine();
-					currentLine++;
-				}else{
-					throw new Exception("Error reading input file!");
-				}
+
+		int currentLine = 0;
+		while(currentLine < lineNumber){
+			if(!input.hasNextLine()){
+				input.close();
+				throw new RuntimeException("Error skipping lines from input file!");
 			}
-		
-			while (input.hasNextLine()) {
-				String newLine = input.nextLine();
-				String[] splittedLine = newLine.split("\t");
+			currentLine++;
+			System.err.println("Skipped line (" + currentLine + "): " + input.nextLine());
+		}
 
-				for (int i = 0; i < splittedLine.length; i++) {
-					splittedLine[i] = splittedLine[i].trim();
-				}
+		while (input.hasNextLine()) {
+			String newLine = input.nextLine();
+			String[] splittedLine = newLine.split("\t");
 
-				String outputLine = "";
-				
-				boolean coletou = false;
-				
-				while(!coletou){
-					try {
-						outputLine = sse.searchSongByTitleAndArtist(
-								splittedLine[2], splittedLine[1], splittedLine[3]);
-						coletou = true;
-					} catch (Exception e) {
-						e.printStackTrace();
-						sse.getProximaAPIKey();
-					}
-					
-				}
-				
+			for (int i = 0; i < splittedLine.length; i++) {
+				splittedLine[i] = splittedLine[i].trim();
+			}
+
+			String outputLine = "";
+
+			boolean coletou = false;
+
+			while(!coletou){
 				try {
 					outputLine = sse.searchSongByTitleAndArtist(
 							splittedLine[2], splittedLine[1], splittedLine[3]);
+					coletou = true;
 				} catch (Exception e) {
+					e.printStackTrace();
 					sse.getProximaAPIKey();
-					outputLine = sse.searchSongByTitleAndArtist(
-							splittedLine[2], splittedLine[1], splittedLine[3]);
 				}
 
-				if (outputLine.isEmpty()) {
-					FileWriter writer = new FileWriter(new File(errorFileName),
-							true);
-					writer.write(newLine + "\n");
-					writer.close();
-				} else {
-					FileWriter writer = new FileWriter(
-							new File(outputFileName), true);
-					StringBuilder sb = new StringBuilder();
-					sb.append(splittedLine[0] + "\t");
-					sb.append(splittedLine[1] + "\t");
-					sb.append(splittedLine[2] + "\t");
-					sb.append(splittedLine[3] + "\t");
-					sb.append(outputLine + "\t");
-					sb.append(splittedLine[4] + "\n");
+			}
 
-					writer.write(sb.toString());
-					writer.close();
-				}
+			if (outputLine.isEmpty()) {
+				FileWriter writer = new FileWriter(new File(errorFileName),
+						true);
+				writer.write(newLine + "\n");
+				writer.close();
+			} else {
+				FileWriter writer = new FileWriter(
+						new File(outputFileName), true);
+				StringBuilder sb = new StringBuilder();
+				sb.append(splittedLine[0] + "\t");
+				sb.append(splittedLine[1] + "\t");
+				sb.append(splittedLine[2] + "\t");
+				sb.append(splittedLine[3] + "\t");
+				sb.append(outputLine + "\t");
+				sb.append(splittedLine[4] + "\n");
 
-				FileWriter writer = new FileWriter(new File("contagem.txt"),
-						false);
-				writer.write("" + (++currentLine));
+				writer.write(sb.toString());
 				writer.close();
 			}
-		} catch (Exception e) {
-			input.close();
+
+			FileWriter writer = new FileWriter(new File("contagem.txt"),
+					false);
+			writer.write("" + (++currentLine));
+			writer.close();
 		}
+
+		input.close();
 	}
 
 }
